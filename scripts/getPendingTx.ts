7efb466db62a7a157573880e63047694c4a84ce2
jakevaughn820 <jakevaughn820@gmail.com>
2022-08-30 20:52:58 -0500

    lots of changes to logging

--- scripts/getPendingTx.ts
+++ scripts/getPendingTx.ts
@@ -3,35 +3,27 @@ import { ethers } from "hardhat"
 var wsProvider = new ethers.providers.WebSocketProvider("ws://127.0.0.1:9944")
 
 async function main() {
-    console.log("Running getPendingTx...")
-
     const rpcProvider = ethers.provider
-    // const blockNum = await wsProvider.getBlockNumber()
-    // const block = await wsProvider.getBlock(blockNum)
-    // console.log(block)
-
     const limCook = await rpcProvider.getSigner(0)
+    const limCookAddr = console.log(await limCook.getAddress())
 
-    console.log(await limCook.getAddress())
+    console.log("Running getPendingTx...")
+    console.log("Bot Signer Address:", limCookAddr)
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`)
 
     wsProvider.on("pending", (tx) => {
         wsProvider.getTransaction(tx).then(async function (transaction?) {
-            if (
-                transaction != null &&
-                transaction.to == "0x59ddC0C8d067dEB508b36d69254Ac6bafD260575"
-            ) {
-                console.log(`transaction: ${transaction.hash}`)
-                // console.log(`gasLimit: ${transaction.gasLimit?.toString()}`)
-                // console.log(`gasPrice: ${transaction.gasPrice?.toString()}`)
-                // console.log(`maxFeePerGas: ${transaction.maxFeePerGas?.toString()}`)
-                // console.log(`maxPriorityFeePerGas: ${transaction.maxPriorityFeePerGas?.toString()}`)
+            console.log(transaction)
 
+            if (transaction != null && transaction.to == "0x59ddC0C8d067dEB508b36d69254Ac6bafD260575") {
                 try {
                     const gasEstimate = await limCook.estimateGas({
                         to: "0xc8367169672C4289797a29Bf8Bc7854804EE39F6",
                         data: transaction.data,
+                        gasLimit: 500000,
+                        maxFeePerGas: 121500000000,
+                        maxPriorityFeePerGas: 2100000000,
                     })
-                    console.log(`Gas Estimate: ${gasEstimate}`)
                     if (gasEstimate.toNumber() < 35000) {
                         const sendTx = await limCook.sendTransaction({
                             to: "0xc8367169672C4289797a29Bf8Bc7854804EE39F6",
@@ -40,11 +32,21 @@ async function main() {
                             maxFeePerGas: 121500000000,
                             maxPriorityFeePerGas: 2100000000,
                         })
-                        console.log(`\nSending my own ${sendTx.hash}`)
+                        const txReceipt = await sendTx.wait()
+                        console.log("BlockNum:", txReceipt.blockNumber)
+                        console.log("tx:", transaction.hash, "\nMirror tx:", txReceipt.transactionHash)
+                        // console.log(`gasLimit: ${transaction.gasLimit}`)
+                        // console.log(`gasPrice: ${transaction.gasPrice}`)
+                        // console.log(`maxFeePerGas: ${transaction.maxFeePerGas}`)
+                        // console.log(`maxPriorityFeePerGas: ${transaction.maxPriorityFeePerGas}`)
+                        console.log("Gas Est:", gasEstimate, "\nGas Used:", txReceipt.gasUsed)
                     }
-                    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`)
+                    console.log(
+                        `~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+                    )
                 } catch (e) {
                     console.log("tx will likely revert")
+                    console.log(`transaction: ${transaction.hash}`)
                 }
             }
         })
--- scripts/mirror0x2372ContractBot.ts
+++ scripts/mirror0x2372ContractBot.ts
@@ -1,58 +1,92 @@
+import { TransactionReceipt, TransactionResponse } from "@ethersproject/providers"
+import { BigNumber } from "ethers"
 import { ethers } from "hardhat"
+import chalk from "chalk"
+import { any } from "hardhat/internal/core/params/argumentTypes"
 
 var wsProvider = new ethers.providers.WebSocketProvider("ws://127.0.0.1:9944")
 
-async function main() {
-    console.log("Running mirror0x2372ContractBot...")
-
+async function mevBot() {
     const rpcProvider = ethers.provider
-    // const blockNum = await wsProvider.getBlockNumber()
-    // const block = await wsProvider.getBlock(blockNum)
-    // console.log(block)
+    const mevBotSigner = await rpcProvider.getSigner(1)
+    const mevBotSignerAddr = await mevBotSigner.getAddress()
 
-    const mirrorBot0x2372 = await rpcProvider.getSigner(1)
+    console.log(
+        chalk.green(`Running mevBot...\n`),
+        `mevBotSignerAddr: ${chalk.cyan(mevBotSignerAddr)}\n`,
+        `Waiting for a memPoolTx Target...`
+    )
 
-    console.log(await mirrorBot0x2372.getAddress())
-
-    wsProvider.on("pending", (tx) => {
-        wsProvider.getTransaction(tx).then(async function (transaction) {
-            if (
-                transaction != null &&
-                transaction.to == "0x2372AA79d0f35310E3Cd3525ecff352922bdAf7C"
-            ) {
-                console.log(`Transaction ${transaction.hash}`)
-                // console.log(`gasLimit: ${transaction.gasLimit?.toString()}`)
-                // console.log(`gasPrice: ${transaction.gasPrice?.toString()}`)
-                // console.log(`maxFeePerGas: ${transaction.maxFeePerGas?.toString()}`)
-                // console.log(`maxPriorityFeePerGas: ${transaction.maxPriorityFeePerGas?.toString()}`)
-
-                const sendTx = await mirrorBot0x2372.sendTransaction({
-                    to: "0x726714e8457aCbD729805223616Ec5A6D8C7193A",
-                    data: transaction.data,
-                    gasLimit: 1000000,
-                    maxFeePerGas: 121500000000,
-                    maxPriorityFeePerGas: 2100000000,
-                })
-                console.log(`Sending my own ${sendTx.hash}`)
-
+    wsProvider.on("pending", (txHash) => {
+        wsProvider.getTransaction(txHash).then(async function (memPoolTx) {
+            if (memPoolTx != null && memPoolTx.to == "0x2372AA79d0f35310E3Cd3525ecff352922bdAf7C") {
                 try {
-                    const gasEstimate = await mirrorBot0x2372.estimateGas({
+                    const mevBotGasEstimate = await mevBotSigner.estimateGas({
                         to: "0x726714e8457aCbD729805223616Ec5A6D8C7193A",
-                        data: transaction.data,
+                        data: memPoolTx.data,
+                        // gasLimit: 1000000,
+                        // maxFeePerGas: 121500000000,
+                        // maxPriorityFeePerGas: 2100000000,
                     })
-                    console.log(`Gas Estimate: ${gasEstimate}`)
-                } catch (e) {
-                    console.log("tx will likely revert")
-                }
 
-                console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`)
+                    if (mevBotGasEstimate.toNumber() > 0) {
+                        const mevBotTx = await mevBotSigner.sendTransaction({
+                            to: "0x726714e8457aCbD729805223616Ec5A6D8C7193A",
+                            data: memPoolTx.data,
+                            gasLimit: 1000000,
+                            maxFeePerGas: 121500000000,
+                            maxPriorityFeePerGas: 2100000000,
+                        })
+
+                        const memPoolTxReceipt = await handleWait(memPoolTx)
+                        const mevBotTxReceipt = await handleWait(mevBotTx)
+                        console.log(
+                            `~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n`,
+                            chalk.green(`Transaction Sent!\n`),
+                            `memPoolTxReceipt blockNumber: ${chalk.yellow(memPoolTxReceipt?.blockNumber)}\n`,
+                            `mevBotTxReceipt blockNumber:  ${chalk.yellow(mevBotTxReceipt?.blockNumber)}\n`,
+                            `memPoolTxReceipt transactionHash: ${chalk.cyan(memPoolTxReceipt?.transactionHash)}\n`,
+                            `mevBotTxReceipt transactionHash:  ${chalk.cyan(mevBotTxReceipt?.transactionHash)}\n`,
+                            `mevBotTx estimateGas:    ${chalk.yellow(mevBotGasEstimate)}\n`,
+                            `mevBotTxReceipt gasUsed: ${chalk.yellow(mevBotTxReceipt?.gasUsed)}\n`,
+                            `memPoolTxReceipt status: ${chalk.yellow(memPoolTxReceipt?.status)}\n`,
+                            `mevBotTxReceipt status:  ${chalk.yellow(mevBotTxReceipt?.status)}\n`
+                        )
+                    }
+                } catch (err) {
+                    const memPoolTxReceipt = await handleWait(memPoolTx)
+
+                    console.log(
+                        `~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n`,
+                        `pending listener failed with error\n`,
+                        err,
+                        chalk.green(`\nMempool Transaction was`),
+                        `memPoolTxReceipt blockNumber: ${chalk.yellow(memPoolTxReceipt?.blockNumber)}\n`,
+                        `memPoolTxReceipt transactionHash: ${chalk.cyan(memPoolTxReceipt?.transactionHash)}\n`,
+                        `memPoolTxReceipt status: ${chalk.yellow(memPoolTxReceipt?.status)}\n`
+                    )
+                }
             }
         })
     })
 }
 
+async function handleWait(tx: TransactionResponse) {
+    try {
+        const txReceipt = await tx.wait()
+        return txReceipt
+    } catch (error: any) {
+        if (error !== null && "receipt" in error) {
+            const txReceipt: TransactionReceipt = error.receipt
+            return txReceipt
+        }
+        console.log(`handleWait failed with error\n`, error)
+        return undefined
+    }
+}
+
 // Able to use async/await everywhere and properly handle errors.
-main().catch((error) => {
+mevBot().catch((error) => {
     console.error(error)
     process.exitCode = 1
 })
