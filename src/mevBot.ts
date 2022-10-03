0d61ce29aecb826456b02cc809924fb4a37e0aed
jakevaughn820 <jakevaughn820@gmail.com>
2022-10-03 16:06:37 -0500

    feat: update to swap

--- src/mevBot.ts
+++ src/mevBot.ts
@@ -6,6 +6,7 @@ import { functionHashes } from "../const/addresses"
 import { wssUrl } from "../hardhat.config"
 import { networkConfig, targetContractItem } from "../helper-hardhat-config"
 import { logHumanReadable } from "./logHumanReadable"
+import { generateRandomNumber } from "./utils/generateRandomNumber"
 import { logger, mevBotTransportFile } from "./utils/logger"
 
 const { ethers, network } = hre
@@ -37,13 +38,8 @@ async function mevBot() {
           for (var i = 0; i <= n; i++) {
             var dataLine = utils.hexDataSlice(memPoolTx.data, i * 32 + 4, (i + 1) * 32 + 4)
             if (dataLine == "0x000000000000000000000000fffffffecb45afd30a637967995394cc88c0c194") {
-              txFound += 6
-              swap(memPoolTx, targetContracts["Target6 Arb"])
-              swap(memPoolTx, targetContracts["Target6 Arb"])
-              swap(memPoolTx, targetContracts["Target6 Arb"])
-              swap2(memPoolTx, targetContracts["Target6 Arb"])
-              swap2(memPoolTx, targetContracts["Target6 Arb"])
-              swap2(memPoolTx, targetContracts["Target6 Arb"])
+              txFound += 2
+              swap(memPoolTx, targetContracts["Target6 Arb"], dataLine)
             }
           }
         }
@@ -155,41 +151,62 @@ async function target3(memPoolTx: TransactionResponse, target: targetContractIte
   }
 }
 
-async function swap(memPoolTx: TransactionResponse, target: targetContractItem) {
+async function swap(memPoolTx: TransactionResponse, target: targetContractItem, asset: string) {
   try {
-    const signerIdx = generateRandomNumber(71, 131)
-    if (signerIdx == undefined) throw new Error("Unknown target.signers[memPoolTx.from]")
+    const dataArray: string[] = []
+    var paramArray: string[]
 
-    const mevBotSigner = ethers.provider.getSigner(signerIdx)
-    const mevBotTx = await mevBotSigner.sendTransaction({
-      to: target.copyContractAddr,
-      gasLimit: 413400,
-      data: "0x68c9718a000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000160000006400000000000000000000000000000000000000640000007b00000000900000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000fffffffecb45afd30a637967995394cc88c0c19400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000022c0d9f000000000bb800000000000000000000000000000000000000000000022c0d9f0000000009c4000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000a049a6260921b5ee3183cfb943133d36d7fdb6680000000000000000000000004efb208eeeb5a8c85af70e8fbc43d6806b422bec",
-      nonce: await mevBotSigner.getTransactionCount(),
-      maxPriorityFeePerGas: memPoolTx.maxPriorityFeePerGas,
-      maxFeePerGas: memPoolTx.maxFeePerGas,
-    })
-    await tempLog(target, memPoolTx, mevBotTx)
-  } catch (err) {
-    await tempErrorLog(err, target, memPoolTx)
-  }
-}
+    paramArray = []
+    paramArray.push("0x68c9718a")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000080")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000100")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000160")
+    paramArray.push("0x000006400000000000000000000000000000000000000640000007b000000009")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000003")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000000")
+    paramArray.push(asset)
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000000")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000002")
+    paramArray.push("0x000000000000000000000000000000000000022c0d9f000000000bb800000000")
+    paramArray.push("0x000000000000000000000000000000000000022c0d9f0000000009c400000000")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000002")
+    paramArray.push("0x000000000000000000000000a049a6260921b5ee3183cfb943133d36d7fdb668")
+    paramArray.push("0x0000000000000000000000004efb208eeeb5a8c85af70e8fbc43d6806b422bec")
+    dataArray.push(utils.hexConcat(paramArray))
 
-async function swap2(memPoolTx: TransactionResponse, target: targetContractItem) {
-  try {
-    const signerIdx = generateRandomNumber(71, 131)
-    if (signerIdx == undefined) throw new Error("Unknown target.signers[memPoolTx.from]")
+    paramArray = []
+    paramArray.push("0x68c9718a")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000080")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000100")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000160")
+    paramArray.push("0x000006400000000000000000000000000000000000000640000007b000000009")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000003")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000000")
+    paramArray.push(asset)
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000000")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000002")
+    paramArray.push("0x000000000000000000000000000000000000022c0d9f0000000009c400000000")
+    paramArray.push("0x000000000000000000000000000000000000022c0d9f000000000bb800000000")
+    paramArray.push("0x0000000000000000000000000000000000000000000000000000000000000002")
+    paramArray.push("0x0000000000000000000000004efb208eeeb5a8c85af70e8fbc43d6806b422bec")
+    paramArray.push("0x000000000000000000000000a049a6260921b5ee3183cfb943133d36d7fdb668")
+    dataArray.push(utils.hexConcat(paramArray))
 
-    const mevBotSigner = ethers.provider.getSigner(signerIdx)
-    const mevBotTx = await mevBotSigner.sendTransaction({
-      to: target.copyContractAddr,
-      gasLimit: 413400,
-      data: "0x68c9718a000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000160000006400000000000000000000000000000000000000640000007b00000000900000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000fffffffecb45afd30a637967995394cc88c0c19400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000022c0d9f0000000009c400000000000000000000000000000000000000000000022c0d9f000000000bb80000000000000000000000000000000000000000000000000000000000000000000000020000000000000000000000004efb208eeeb5a8c85af70e8fbc43d6806b422bec000000000000000000000000a049a6260921b5ee3183cfb943133d36d7fdb668",
-      nonce: await mevBotSigner.getTransactionCount(),
-      maxPriorityFeePerGas: memPoolTx.maxPriorityFeePerGas,
-      maxFeePerGas: memPoolTx.maxFeePerGas,
-    })
-    await tempLog(target, memPoolTx, mevBotTx)
+    for (const data in dataArray) {
+      const signerIdx = generateRandomNumber(71, 131)
+      if (signerIdx == undefined) throw new Error("Unknown target.signers[memPoolTx.from]")
+      const mevBotSigner = ethers.provider.getSigner(signerIdx)
+      const mevBotTx = await mevBotSigner.sendTransaction({
+        to: target.copyContractAddr,
+        gasLimit: 413400,
+        data: data,
+        nonce: await mevBotSigner.getTransactionCount(),
+        maxPriorityFeePerGas: memPoolTx.maxPriorityFeePerGas,
+        maxFeePerGas: memPoolTx.maxFeePerGas,
+        gasPrice: memPoolTx.maxFeePerGas ? undefined : memPoolTx.gasPrice,
+      })
+      await tempLog(target, memPoolTx, mevBotTx)
+    }
   } catch (err) {
     await tempErrorLog(err, target, memPoolTx)
   }
@@ -221,12 +238,6 @@ async function tempErrorLog(err: unknown, target: targetContractItem, memPoolTx:
     signer: target.signers[memPoolTx.from],
     error: err,
   })
-}
-
-const generateRandomNumber = (min: number, max: number) => {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 mevBot().catch(error => {
