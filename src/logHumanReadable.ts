ba5fbfd0ab33343e76c1977bde863d74fd5716f0
jakevaughn820 <jakevaughn820@gmail.com>
2022-10-15 23:37:39 -0500

    chore: small change to delay

--- src/logHumanReadable.ts
+++ src/logHumanReadable.ts
@@ -56,7 +56,7 @@ export async function logHumanReadable(info: any, hre: HardhatRuntimeEnvironment
       return
     }
 
-    await delay(60000 + ((md.txReported * 100) % 100000))
+    await delay(((md.txReported * 1000) % 120000) + 60000)
     const [receipt1, receipt2] = await Promise.all([
       receiptWaitHandler(md.memPoolTx, md.blockFound, hre),
       receiptWaitHandler(md.mevBotTx, md.blockFound, hre),
